1d2c2f81e89c663fab22f7511cfb8a634d8f7ab9
Anirudh1411 <anirudhragu210@gmail.com>
2020-11-05 17:40:06 +0530

    Updates

---
+++ precache-manifest.e5d192f2abc9a8ed687bfe88dfb45a72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c03f6af9a9075c02fd1e5a9d122e37d",
+    "url": "/CoronaCases/index.html"
+  },
+  {
+    "revision": "b6e91a8af25beea4b1c8",
+    "url": "/CoronaCases/static/css/main.4e637d47.chunk.css"
+  },
+  {
+    "revision": "8ccaa7f67d1e933ed3ef",
+    "url": "/CoronaCases/static/js/2.e992256f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CoronaCases/static/js/2.e992256f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6e91a8af25beea4b1c8",
+    "url": "/CoronaCases/static/js/main.c4a19100.chunk.js"
+  },
+  {
+    "revision": "7e9ebe5f2b25c90439cd",
+    "url": "/CoronaCases/static/js/runtime-main.37a11371.js"
+  },
+  {
+    "revision": "127012906c3407d6b6140a1e89bd6ab7",
+    "url": "/CoronaCases/static/media/favicon.12701290.png"
+  }
+]);
